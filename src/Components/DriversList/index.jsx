66820b171b5fe59889dd0e7af427eedcd0a2b0bc
Matheus Link <linksauermths2203@gmail.com>
2024-03-03 23:38:14 -0300

    data: setting the drivers api consumption

--- src/Components/DriversList/index.jsx
+++ src/Components/DriversList/index.jsx
@@ -1,21 +1,12 @@
-import { useState, useEffect } from "react";
-import xmlParser from "xml-js";
+const DriversList = ({drivers}) => {
 
-const DriversList = () => {
-  const [drivers, setDrivers] = useState([]);
-
-  useEffect(() => {
-    
-    }, []);
-    
-    console.log(drivers);
   return (
     <main>
       {drivers.map((driver, index) => {
         return (
           <section key={index}>
             <h3>
-              {driver.GivenName._text} {driver.FamilyName._text}
+              {driver.full_name}
             </h3>
           </section>
         );
--- src/Pages/DriversPage/index.jsx
+++ src/Pages/DriversPage/index.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import NavBar from "../../Components/NavBar";
+import { fetchDrivers } from "../../Services/Api";
+import DriversList from "../../Components/DriversList";
 
 const DriversPage = () => {
+    const [drivers, setDrivers] = useState([]);
+
+    useEffect(() => {
+      async function fetchData() {
+        const data = await fetchDrivers();
+        setDrivers(data.slice(2200, 2220));
+      }
+
+      fetchData();
+    }, []);
+
+    console.log(drivers);
+    
+
   return (
     <main>
       <NavBar /> DriversPage
+      <DriversList drivers={drivers}/>
     </main>
   );
 };
