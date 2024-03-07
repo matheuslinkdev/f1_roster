e7941a44f821e24e4e3fb8183e876b7be1e8749f
Matheus Link <linksauermths2203@gmail.com>
2024-03-07 19:07:44 -0300

    Styles: Styling the NavBar

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -3,12 +3,13 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
+import './style.scss'
 
 import f1_roster_icon from "/public/icons/f1_roster-icon.png";
 
 function NavBar() {
   return (
-    <Navbar expand="lg" style={{ backgroundColor: "#e11001" }}>
+    <Navbar expand="lg"className="navbar">
       <Container>
         <img
           src={f1_roster_icon}
@@ -18,22 +19,20 @@ function NavBar() {
             maxHeight: "5rem",
           }}
         />
-        <Navbar.Brand href="/">F1 Roster</Navbar.Brand>
+        <Navbar.Brand href="/" className="brand">
+          F1 Roster
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/standings">Standings</Nav.Link>
-            <Nav.Link href="/results">Results</Nav.Link>
+          <Nav className="links-nav">
+            <Nav.Link className="link" href="/">Home</Nav.Link>
+            <Nav.Link className="link" href="/standings">Standings</Nav.Link>
+            <Nav.Link className="link" href="/results">Results</Nav.Link>
             <NavDropdown title="Infos" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/drivers">Drivers</NavDropdown.Item>
-              <NavDropdown.Item href="/schedule">Schedule</NavDropdown.Item>
-              <NavDropdown.Item href="/constructors">
+              <NavDropdown.Item className="dropdown-link" href="/drivers">Drivers</NavDropdown.Item>
+              <NavDropdown.Item className="dropdown-link" href="/schedule">Schedule</NavDropdown.Item>
+              <NavDropdown.Item className="dropdown-link" href="/constructors">
                 Constructors
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
