a12b63c17e84dc5c7ed1b0b1d3f3aca76e829ccc
Matheus Link <linksauermths2203@gmail.com>
2024-03-14 10:01:57 -0300

    Updating the routing links

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -3,9 +3,10 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './style.scss'
+import "./style.scss";
 
 import f1_roster_icon from "/public/icons/f1_roster-icon.png";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
@@ -19,39 +20,36 @@ function NavBar() {
             maxHeight: "5rem",
           }}
         />
-        <Navbar.Brand href="/f1_roster" className="brand">
+        <Navbar.Brand href="/f1_roster/" className="brand">
           F1 Roster
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="links-nav">
-            <Nav.Link className="link" href="/f1_roster/">
-              Home
+            <Nav.Link>
+              <Link to="/f1_roster/" className="link">
+                Home
+              </Link>
             </Nav.Link>
-            <Nav.Link className="link" href="/f1_roster/standings">
-              Standings
+            <Nav.Link>
+              <Link to="/f1_roster/standings" className="link">
+                Standings
+              </Link>
             </Nav.Link>
-            <Nav.Link className="link" href="/f1_roster/results">
-              Results
+            <Nav.Link>
+              <Link to="/f1_roster/results" className="link">
+                Results
+              </Link>
             </Nav.Link>
             <NavDropdown title="Infos" id="basic-nav-dropdown">
-              <NavDropdown.Item
-                className="dropdown-link"
-                href="/f1_roster/drivers"
-              >
-                Drivers
+              <NavDropdown.Item className="dropdown-link">
+                <Link to="/f1_roster/drivers">Drivers</Link>
               </NavDropdown.Item>
-              <NavDropdown.Item
-                className="dropdown-link"
-                href="/f1_roster/schedule"
-              >
-                Schedule
+              <NavDropdown.Item className="dropdown-link">
+                <Link to="/f1_roster/schedule">Schedule</Link>
               </NavDropdown.Item>
-              <NavDropdown.Item
-                className="dropdown-link"
-                href="/f1_roster/constructors"
-              >
-                Constructors
+              <NavDropdown.Item className="dropdown-link">
+                <Link to="/f1_roster/constructors">Constructors</Link>
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
