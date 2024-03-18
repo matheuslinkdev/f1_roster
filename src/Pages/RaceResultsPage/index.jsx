0e04919f398f9bde40d16b8de279fe9407b11bc3
Matheus Link <linksauermths2203@gmail.com>
2024-03-17 21:12:08 -0300

    Adding memo to the resultsPage and fixing a conflitant style issue

--- src/Pages/RaceResultsPage/index.jsx
+++ src/Pages/RaceResultsPage/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import xmlParser from "xml-js";
 import RaceResultsList from "../../Components/ResultsList";
 import NavBar from "../../Components/NavBar";
@@ -8,45 +8,49 @@ const RaceResultsPage = () => {
   const [raceResults, setRaceResults] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-  const fetchRaceResults = async () => {
-    try {
-      setIsLoading(true);
-
-      const results = await Promise.all(
-        Array.from({ length: 25 }, async (_, i) => {
-          const response = await fetch(
-            `https://ergast.com/api/f1/current/${i + 1}/results`
-          );
-          if (!response.ok) {
-            throw new Error("Error fetching race data  " + (i + 1));
-          }
-          const data = await response.text();
-          const json = xmlParser.xml2js(data, { compact: true, spaces: 4 });
-          return json.MRData;
-        })
-      );
-      setRaceResults(results);
-    } catch (error) {
-      console.error("Error fetching race results", error);
-    } finally {
-      setIsLoading(false);
-    }
+  const fetchRaceResults = async (raceCount) => {
+    const results = await Promise.all(
+      Array.from({ length: raceCount }, async (_, i) => {
+        const response = await fetch(
+          `https://ergast.com/api/f1/current/${i + 1}/results`
+        );
+        if (!response.ok) {
+          throw new Error("Error fetching race data  " + (i + 1));
+        }
+        const data = await response.text();
+        const json = xmlParser.xml2js(data, { compact: true, spaces: 4 });
+        return json.MRData;
+      })
+    );
+    return results;
   };
 
+  useEffect(() => {
+    const loadRaceResults = async () => {
+      try {
+        setIsLoading(true);
 
-    fetchRaceResults();
+        const results = await fetchRaceResults(25);
+        setRaceResults(results);
+      } catch (error) {
+        console.error("Error fetching race results", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadRaceResults();
   }, []);
 
-console.log(raceResults);
+  const memoizedRaceResults = useMemo(() => raceResults, [raceResults]);
 
   return (
     <main>
       <NavBar />
       {isLoading ? (
-        <Loading/>
+        <Loading />
       ) : (
-        <RaceResultsList raceResults={raceResults} isLoading={isLoading} />
+        <RaceResultsList raceResults={memoizedRaceResults} />
       )}
     </main>
   );
