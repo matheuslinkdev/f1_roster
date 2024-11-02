035c73fa8be3839854919f801c123d82e6f0db5e
Matheus Link <linksauermths2203@gmail.com>
2024-11-01 22:21:59 -0300

    feat(page): Creates team standings route
    -Creates a route to show the constructor standings route
    -Creates function that deal with the related data
    -Creates constructor card for standings

--- src/app/formulaone/constructor-standings/page.tsx
+++ src/app/formulaone/constructor-standings/page.tsx
@@ -1,12 +1,19 @@
 import { GetConstructorStandings } from '@/api/FetchF1Data'
 import React from 'react'
+import ConstructorsStandingsData from './standings';
 
 const ConstructorStandings = async () => {
-  const standings = await GetConstructorStandings()
 
   return (
-    <main className='flex flex-col w-full min-h-screen'>ConstructorStandings</main>
-  )
+    <main className="flex flex-col w-full min-h-screen py-8">
+      <article className="flex flex-col gap-4 my-4">
+        <h1 className="text-3xl">Formula 1 2024 Constructor Standings</h1>
+      </article>
+      <article className="flex flex-col justify-center items-center gap-8">
+        <ConstructorsStandingsData />
+      </article>
+    </main>
+  );
 }
 
 export default ConstructorStandings
